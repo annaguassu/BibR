c2d98e92f9dfd1f665bd24b794682e8dc49e8b1c
aprendiz.ti <aprendiz.ti@SUPLEY01-NOTE>
2022-04-25 11:39:37 -0300

    Adicionar arquivos de projeto.

---
+++ BibR/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibR.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ BibR/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace BibR.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string tel { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool terms { get; set; }
+
+    }
+}
