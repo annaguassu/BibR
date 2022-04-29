bc1d9ec7aecc71f9cf55cd0f00cec53bb3a230f3
ANA LOPES <ana.lopes28@fatec.sp.gov.br>
2022-04-29 10:30:47 -0300

    29/04

--- BibR/wwwroot/js/site.js
+++ BibR/wwwroot/js/site.js
@@ -1,4 +1,59 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//mostrar senha
+let mostrarsenha = document.querySelector('.mostrar');
+let ocultarsenha = document.querySelector('.ocultar');
+let mostrarsenha2 = document.querySelector('.mostrar2');
+let ocultarsenha2 = document.querySelector('.ocultar2');
+let input = document.getElementById('senha');
+let inputs = document.getElementById('senha2');
 
-// Write your JavaScript code.
+mostrarsenha.addEventListener('click', (e) => {
+    input.setAttribute('type', 'text');
+    mostrarsenha.style.display = 'none';
+    ocultarsenha.style.display = 'block';
+});
+
+
+ocultarsenha.addEventListener('click', (e) => {
+    input.setAttribute('type', 'password');
+    mostrarsenha.style.display = 'block';
+    ocultarsenha.style.display = 'none';
+});
+mostrarsenha.addEventListener('click', (e) => {
+    inputs.setAttribute('type', 'text');
+    mostrarsenha.style.display = 'none';
+    ocultarsenha.style.display = 'block';
+});
+
+
+ocultarsenha.addEventListener('click', (e) => {
+    inputs.setAttribute('type', 'password');
+    mostrarsenha.style.display = 'block';
+    ocultarsenha.style.display = 'none';
+});
+
+mostrarsenha2.addEventListener('click', (e) => {
+    input.setAttribute('type', 'text');
+    mostrarsenha.style.display = 'none';
+    ocultarsenha.style.display = 'block';
+});
+
+
+ocultarsenha2.addEventListener('click', (e) => {
+    input.setAttribute('type', 'password');
+    mostrarsenha.style.display = 'block';
+    ocultarsenha.style.display = 'none';
+});
+mostrarsenha2.addEventListener('click', (e) => {
+    inputs.setAttribute('type', 'text');
+    mostrarsenha.style.display = 'none';
+    ocultarsenha.style.display = 'block';
+});
+
+
+ocultarsenha2.addEventListener('click', (e) => {
+    inputs.setAttribute('type', 'password');
+    mostrarsenha.style.display = 'block';
+    ocultarsenha.style.display = 'none';
+});
+//-----------------------------------
