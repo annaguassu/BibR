7678275cb9475e3d3ea06c6d68803f031c3659a3
ANA LOPES <ana.lopes28@fatec.sp.gov.br>
2022-05-09 12:22:48 -0300

    09/05/2022

---
+++ BibR/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibR.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Logar()
+        {
+            return View();
+        }
+    }
+}
